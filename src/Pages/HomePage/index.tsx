dbf3ba3c01ae251263bdd0e9a3d52c846d73e37b
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-06 01:15:53 -0300

    Add Footer In home

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,10 +1,11 @@
-import Home from '../../Containers/Home'
+import Footer from '../../Containers/Footer';
+import Form from '../../Containers/Form';
+import Home from '../../Containers/Home';
 import HowWeBuild from '../../Containers/HowWeBuild';
-import Introduction from '../../Containers/Introduction'
+import Introduction from '../../Containers/Introduction';
 import Technologies from '../../Containers/Technologies';
 import WhyBuild from '../../Containers/WhyBuild';
 import { Container } from './styled';
-
 
 export default function HomePage() {
     return (
@@ -14,6 +15,8 @@ export default function HomePage() {
             <WhyBuild />
             <Technologies />
             <HowWeBuild />
+            <Form />
+            <Footer />
         </Container>
     );
 }
--- src/Pages/HomePage/styled.tsx
+++ src/Pages/HomePage/styled.tsx
@@ -7,4 +7,3 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
 `;
-

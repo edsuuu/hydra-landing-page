100710a3a55c6739b621dc482221d6fc27a2a9af
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-06 01:15:45 -0300

    Add Footer

---
+++ src/Containers/Footer/index.tsx
@@ -0,0 +1,84 @@
+import { Buttons, Container, SocialFooter } from './styled';
+import Button from '../../Components/Button';
+import logoVetor from '../../assets/logo-vetor.png';
+import lineVetor from '../../assets/line-vetor.png';
+import lineLong from '../../assets/line-long.png';
+import facebook from '../../assets/facebook.png';
+import twitter from '../../assets/twitter.png';
+import linkedin from '../../assets/linkedin.png';
+import youtube from '../../assets/youtube.png';
+import instagram from '../../assets/instagram.png';
+import pinterest from '../../assets/pinterest.png';
+
+export default function Footer() {
+    const links = [
+        { id: 1, name: 'About', to: '#' },
+        { id: 2, name: 'Services', to: '#' },
+        { id: 3, name: 'Technologies', to: '#' },
+        { id: 4, name: 'How To', to: '#' },
+        { id: 5, name: 'Join Hydra', to: '#' },
+        { id: 6, name: 'F.A.Q', to: '#' },
+        { id: 7, name: 'Sitemap', to: '#' },
+        { id: 8, name: 'Conditions', to: '#' },
+        { id: 9, name: 'Licenses', to: '#' },
+    ];
+
+    const socialLinks = [
+        { id: 1, image: facebook, imageAlt: 'facebook', to: '#' },
+        { id: 2, image: twitter, imageAlt: 'twitter', to: '#' },
+        { id: 3, image: linkedin, imageAlt: 'linkedin', to: '#' },
+        { id: 4, image: youtube, imageAlt: 'youtube', to: '#' },
+        { id: 5, image: instagram, imageAlt: 'instagram', to: '#' },
+        { id: 6, image: pinterest, imageAlt: 'pinterest', to: '#' },
+    ];
+
+    return (
+        <>
+            <Container>
+                <div>
+                    <img src={logoVetor} alt="logoVetor" />
+                </div>
+                <div>
+                    <img src={lineVetor} alt="lineVetor" />
+                </div>
+                <div>
+                    {links.slice(0, 5).map((link) => (
+                        <p key={link.id}> {link.name}</p>
+                    ))}
+                </div>
+                <div>
+                    <img src={lineVetor} alt="lineVetor" />
+                </div>
+                <div>
+                    {links.slice(5).map((link) => (
+                        <p key={link.id}> {link.name}</p>
+                    ))}
+                </div>
+                <div>
+                    <img src={lineVetor} alt="lineVetor" />
+                </div>
+                <SocialFooter>
+                    <div className="title-footer">
+                        <h3>SOCIALIZE WITH HYDRA</h3>
+                    </div>
+                    <div>
+                        {socialLinks.map((socialLink) => (
+                            <a key={socialLink.id} href={socialLink.to} target="_blank" rel="noreferrer">
+                                <img src={socialLink.image} alt={socialLink.imageAlt} />
+                            </a>
+                        ))}
+                    </div>
+                    <Buttons>
+                        <Button text="BUILD YOU WORLD"></Button>
+                    </Buttons>
+                </SocialFooter>
+            </Container>
+            <div>
+                <img src={lineLong} alt="" />
+            </div>
+            <div className="credits">
+                <h2>2024 © HYDRA LANDING PAGE - DEVELOPED BY EDSON - ALL RIGHTS RESERVED </h2>
+            </div>
+        </>
+    );
+}
---
+++ src/Containers/Footer/styled.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+`;
+
+export const Buttons = styled.div`
+    button {
+        font-weight: bold;
+        font-family: 'Montserrat', sans-serif;
+        text-transform: uppercase;
+        width: auto;
+
+        padding: 1rem 2rem;
+        border-radius: 50px;
+        cursor: pointer;
+    }
+
+    button:nth-child(1) {
+        border: none;
+        color: #302c42;
+        background: linear-gradient(to right, #8176af, #c0b7e8);
+        transition: 0.3s ease-in;
+
+        &:hover {
+            background: #ffffff;
+        }
+    }
+`;
+
+export const SocialFooter = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    div:nth-child(2) {
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+    }
+`;

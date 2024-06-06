fe2caaee0d1ff9473b24082d89f87d673b66e428
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-06 19:57:51 -0300

    Pre fix

--- src/Components/ContactHome/index.tsx
+++ src/Components/ContactHome/index.tsx
@@ -1,59 +1,49 @@
-import { Container, IntroductionContactHome, BoxIntroductionContactHome } from './styled';
+import { IntroductionContactHome, BoxIntroductionContactHome } from './styled';
 import location from '../../assets/location.svg';
 import phoneCall from '../../assets/phone.svg';
 import emailImg from '../../assets/mail.svg';
 import lineVertical from '../../assets/line-vertical.png';
 
-interface ContactHomeProps {
-    id: number;
-    title: string;
-    description: string;
-    url: string;
-    line?: string;
-}
-
-const contacts: ContactHomeProps[] = [
-    {
-        id: 1,
-        title: 'Pay Us a Visit',
-        description: 'Union St, Seattle, WA 98101, United States',
-        url: phoneCall,
-        line: lineVertical,
-    },
-    {
-        id: 2,
-        title: 'Give Us a Call',
-        description: '(110) 1111-1010',
-        url: location,
-        line: lineVertical,
-    },
-    {
-        id: 3,
-        title: 'Send Us a Message',
-        description: 'Union St, Seattle, WA 98101, United States',
-        url: emailImg,
-    },
-];
+// interface ContactHomeProps {
+//     id: number;
+//     title: string;
+//     description: string;
+//     url: string;
+//     line?: string;
+// }
 
 export default function ContactPageHome() {
     return (
-        <Container>
-            <IntroductionContactHome>
-                {contacts.map((socialLink) => (
-                    <BoxIntroductionContactHome key={socialLink.id}>
-                        <div>
-                            <img src={socialLink.url} alt={socialLink.title} />
-                        </div>
-                        <div>
-                            <h1>{socialLink.title}</h1>
-                            <p>{socialLink.description}</p>
-                        </div>
-                        <div>
-                            <img src={socialLink.line} alt={socialLink.line ? socialLink.title : ''}/>
-                        </div>
-                    </BoxIntroductionContactHome>
-                ))}
-            </IntroductionContactHome>
-        </Container>
+        <IntroductionContactHome>
+            <BoxIntroductionContactHome className='location'>
+                <div>
+                    <img src={location} alt={'location'} />
+                </div>
+                <div>
+                    <h1>Pay Us a Visit</h1>
+                    <p>Union St, Seattle, WA 98101, United States</p>
+                </div>
+            </BoxIntroductionContactHome>
+            <img className='line' src={lineVertical} alt="lineVertical" />
+            <BoxIntroductionContactHome className='phoneCall'>
+                <div>
+                    <img src={phoneCall} alt={'phoneCall'} />
+                </div>
+                <div>
+                    <h1>Give Us a Call</h1>
+                    <p>{'(110) 1111-1010'}</p>
+                </div>
+            </BoxIntroductionContactHome>
+            <img className='line' src={lineVertical} alt="lineVertical" />
+            <BoxIntroductionContactHome className='emailImg'>
+                <div>
+                    <img src={emailImg} alt={'emailImg'} />
+                </div>
+                <div>
+                    <h1>Send Us a Message</h1>
+                    <p>Contact@HydraTech.com</p>
+                </div>
+            </BoxIntroductionContactHome>
+        </IntroductionContactHome>
     );
 }
--- src/Components/ContactHome/styled.tsx
+++ src/Components/ContactHome/styled.tsx
@@ -1,25 +1,59 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
-
 export const IntroductionContactHome = styled.div`
+    margin-top: 3rem;
     width: 100%;
     display: flex;
     flex-direction: row;
-    justify-content: center;
-    gap: 2rem;
+    justify-content: space-evenly;
+    /* gap: 1rem; */
+    padding: 1rem 0rem;
     background: radial-gradient(circle, #3a3456, #211e2e);
     border-radius: 5rem;
+
+    @media (max-width: 1100px) {
+        margin-left: auto;
+        margin-right: auto;
+        width: 95%;
+
+        .line {
+            display: none;
+        }
+        .emailImg {
+            display: none;
+        }
+        .phoneCall {
+            display: none;
+        }
+        .location {
+            /* display: none; */
+        }
+    }
 `;
 
 export const BoxIntroductionContactHome = styled.div`
-    width: 100%;
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-evenly;
-`;
+    justify-content: center;
+    padding: 10px;
+    border-radius: 4rem;
+    gap: 0.5rem;
+    font-family: 'Montserrat', sans-serif;
+    color: #ffffff;
+    h1 {
+        margin: 10px;
+        font-size: 1.4rem;
+    }
+    p {
+        margin: 10px;
+        font-size: 1rem;
+    }
+    img {
+        width: 50px;
+        height: 54px;
+    }
 
-export const Conteudo = styled.div`
-    border: 1px solid #00eeff;
+    @media (max-width: 500px) {
+    }
 `;

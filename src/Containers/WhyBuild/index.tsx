0b8b910763c926846e6622a078cabcbacca85a34
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-07 16:06:44 -0300

    Update index.tsx

--- src/Containers/WhyBuild/index.tsx
+++ src/Containers/WhyBuild/index.tsx
@@ -1,45 +1,46 @@
-import { Card } from './styled';
+import { Card, Container } from './styled';
 import simulation from '../../assets/simulation.png';
 import education from '../../assets/education.png';
 import selfCare from '../../assets/selfcare.png';
 import outdoor from '../../assets/outdoor.png';
 import arrowLong from '../../assets/arrow-from-right-long.png';
 import TitleComponent from '../../Components/TitleComponent';
-import { TitleProps } from '../../utils/interfaces';
+import { TitleProps, LineObjProps } from '../../utils/interfaces';
+import CardBuildComponent from '../../Components/CardBuildComponent';
 // import Button from '../../Components/Button';
+import lineHorizont from '../../assets/line-horizont-card.png';
+
+const lineObj: LineObjProps[] = [
+    {
+        url: lineHorizont,
+        alt: 'lineHorizont',
+    },
+];
 
 const card = [
     {
-        id: 1,
         image: simulation,
         imageAlt: 'SIMULATION',
         name: 'SIMULATION',
         paragraph: 'Vitae sapien pellentesque habitant morbi nunc. Viverra aliquet  porttitor rhoncus libero justo laoreet sit amet vitae.',
-        buttonText: 'TRY IT NOW',
     },
     {
-        id: 2,
         image: education,
         imageAlt: 'EDUCATION',
         name: 'EDUCATION',
         paragraph: 'Vitae sapien pellentesque habitant morbi nunc. Viverra aliquet  porttitor rhoncus libero justo laoreet sit amet vitae.',
-        buttonText: 'TRY IT NOW',
     },
     {
-        id: 3,
         image: selfCare,
         imageAlt: 'SELF-CARE',
         name: 'SELF-CARE',
         paragraph: 'Vitae sapien pellentesque habitant morbi nunc. Viverra aliquet  porttitor rhoncus libero justo laoreet sit amet vitae.',
-        buttonText: 'TRY IT NOW',
     },
     {
-        id: 4,
         image: outdoor,
         imageAlt: 'OUTDOOR',
         name: 'OUTDOOR',
         paragraph: 'Vitae sapien pellentesque habitant morbi nunc. Viverra aliquet  porttitor rhoncus libero justo laoreet sit amet vitae.',
-        buttonText: 'TRY IT NOW',
     },
 ];
 
@@ -55,21 +56,23 @@ const titleObj: TitleProps[] = [
 
 export default function WhyBuild() {
     const title: TitleProps = titleObj[0];
+    const line: LineObjProps = lineObj[0];
 
     return (
-        <div>
+        <Container>
             <TitleComponent title={title.title} subTitle={title.subTitle} paragraph={title.paragraph} imgArrow={title.imgArrow} />
             <Card>
-                {card.map((item) => (
-                    <div key={item.id}>
-                        <img src={item.image} alt={item.imageAlt} />
-                        <h3>{item.name}</h3>
-                        <p>{item.paragraph}</p>
+                {card.map((item, index) => (
+                    <CardBuildComponent key={index} title={item.name} image={item.image} imageAlt={item.imageAlt} url2={line.url} alt2={line.alt} paragraph={item.paragraph} />
 
-                        {/* <Button text={item.buttonText}></Button> */}
-                    </div>
+                    // <div key={item.id}>
+                    //     <img src={item.image} alt={item.imageAlt} />
+                    //     <h3>{item.name}</h3>
+                    //     <p>{item.paragraph}</p>
+
+                    // </div>
                 ))}
             </Card>
-        </div>
+        </Container>
     );
 }
